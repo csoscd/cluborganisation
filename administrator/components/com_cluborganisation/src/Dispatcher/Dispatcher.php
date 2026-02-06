e94f9632c30e498cb2edc33f323f2630e75053e1
csoscd <37827904+csoscd@users.noreply.github.com>
2026-02-06 08:44:23 +0100

    Add build script for installable Joomla package

---
+++ administrator/components/com_cluborganisation/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cluborganisation
+ *
+ * @copyright   Copyright (C) 2025 Christian Schulz
+ * @license     GPL-2.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace Joomla\Component\Cluborganisation\Administrator\Dispatcher;
+
+use Joomla\CMS\Dispatcher\ComponentDispatcher;
+
+/**
+ * Component dispatcher.
+ */
+class Dispatcher extends ComponentDispatcher
+{
+}
---
+++ components/com_cluborganisation/tmpl/activememberships/default.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_cluborganisation
+ */
+
+declare(strict_types=1);
+
+use Joomla\CMS\Language\Text;
+
+$labels = [
+    'person.firstname' => Text::_('COM_CLUBORGANISATION_FIELD_FIRSTNAME_LABEL'),
+    'person.lastname' => Text::_('COM_CLUBORGANISATION_FIELD_LASTNAME_LABEL'),
+    'person.member_no' => Text::_('COM_CLUBORGANISATION_FIELD_MEMBER_NO_LABEL'),
+    'membership.type' => Text::_('COM_CLUBORGANISATION_FIELD_MEMBERSHIP_TYPE_LABEL'),
+    'membership.begin' => Text::_('COM_CLUBORGANISATION_FIELD_BEGIN_LABEL'),
+    'membership.end' => Text::_('COM_CLUBORGANISATION_FIELD_END_LABEL'),
+];
+
+$fields = $this->fields ?: array_keys($labels);
+?>
+<h1><?php echo Text::_('COM_CLUBORGANISATION_ACTIVE_MEMBERSHIPS'); ?></h1>
+<?php if (!$this->items) : ?>
+    <p><?php echo Text::_('COM_CLUBORGANISATION_NO_DATA'); ?></p>
+<?php else : ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <?php foreach ($fields as $field) : ?>
+                    <th><?php echo $labels[$field] ?? $field; ?></th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($this->items as $item) : ?>
+                <tr>
+                    <?php foreach ($fields as $field) : ?>
+                        <td>
+                            <?php if ($field === 'person.firstname') : ?>
+                                <?php echo $this->escape($item->firstname); ?>
+                            <?php elseif ($field === 'person.lastname') : ?>
+                                <?php echo $this->escape($item->lastname); ?>
+                            <?php elseif ($field === 'person.member_no') : ?>
+                                <?php echo $this->escape($item->member_no); ?>
+                            <?php elseif ($field === 'membership.type') : ?>
+                                <?php echo $this->escape((string) $item->membership_type_title); ?>
+                            <?php elseif ($field === 'membership.begin') : ?>
+                                <?php echo $this->escape($item->begin); ?>
+                            <?php elseif ($field === 'membership.end') : ?>
+                                <?php echo $this->escape($item->end); ?>
+                            <?php endif; ?>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif; ?>
