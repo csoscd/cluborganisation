c62147438787094733c36559dd140b0ff0e04cb1
csoscd <37827904+csoscd@users.noreply.github.com>
2026-02-06 09:56:19 +0100

    Map legacy controller names to class names

---
+++ administrator/components/com_cluborganisation/src/Table/PersonTable.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cluborganisation
+ *
+ * @copyright   Copyright (C) 2025 Christian Schulz
+ * @license     GPL-2.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace Joomla\Component\Cluborganisation\Administrator\Table;
+
+use Joomla\CMS\Table\Table;
+use Joomla\Database\DatabaseDriver;
+
+/**
+ * Table class for persons.
+ */
+class PersonTable extends Table
+{
+    /**
+     * Constructor.
+     *
+     * @param   DatabaseDriver  $db  The database connector object.
+     */
+    public function __construct(DatabaseDriver $db)
+    {
+        parent::__construct('#__cluborganisation_persons', 'id', $db);
+    }
+}
---
+++ administrator/components/com_cluborganisation/src/View/Membership/HtmlView.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cluborganisation
+ *
+ * @copyright   Copyright (C) 2025 Christian Schulz
+ * @license     GPL-2.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace Joomla\Component\Cluborganisation\Administrator\View\Membership;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * HTML view for Membership form.
+ */
+class HtmlView extends BaseHtmlView
+{
+    /**
+     * @var  \JForm
+     */
+    protected ;
+
+    /**
+     * @var  object
+     */
+    protected ;
+
+    /**
+     * Display the view.
+     *
+     * @param   string    Template name.
+     *
+     * @return  void
+     */
+    public function display( = null): void
+    {
+        ->form = ->get('Form');
+        ->item = ->get('Item');
+
+        parent::display();
+    }
+}
